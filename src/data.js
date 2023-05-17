7819e7e400119d03a01c0b7f4cc43ac83879727c
mbatorek7 <maegan.batorek@valpo.edu>
2023-05-16 22:09:44 -0500

    updating nav, skills, and contact

--- src/data.js
+++ src/data.js
@@ -1,34 +1,44 @@
 export const projects = [
-    {
-      title: "Team Profile",
-      subtitle: "Inquirer and Tests",
-      description:
-        "With this app, the user can easily create a team profile that can consist of manager(s), engineer(s), and intern(s). The web app created will displayed all necessary information and allow the user to email any employee that it chooses with the press of a button.",
-      image: "./project-1.gif",
-      link: "https://github.com/mbatorek7/hw10-team-profile",
-    },
-    {
-      title: "Note Taker",
-      subtitle: "Express",
-      description:
-        "With this application, the user can create a list of notes. Each note is displayed on the side of the screen. When the user clicks on the note, they can see the title and text that was written. The user's notes will be saved unless the user presses the delete button.",
-      image: "./project-2.gif",
-      link: "https://batorek-note-taker.herokuapp.com/",
-    },
-    {
-      title: "E-commerce Back End",
-      subtitle: "Object-relational mapping",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-3.gif",
-      link: "https://github.com/mbatorek7/hw13-ORM",
-    },
-    {
-      title: "Work Day Scheduler",
-      subtitle: "Third-Party APIs",
-      description:
-        "This application is designed to let you easily manage your busy workday schedule. It allows you to see the exact date and time at the top. With that in mind, you will also be able to see which timeslots have past, which one is the present, and which timeslots are upcoming based on the colors. The user can input their tasks into a timeslot, but the task will not be saved unless the user presses the save button. Once saved, the user can close the application and reopen it later to see their tasks. If the user wishes to clear their schedule for the day, they can do so with the clear schedule button at the top of the page.",
-      image: "./project-4.gif",
-      link: "https://mbatorek7.github.io/hw5-workday-scheduler/",
-    },
-  ];
+  {
+    title: "Team Profile",
+    subtitle: "Inquirer and Tests",
+    description:
+      "With this app, the user can easily create a team profile that can consist of manager(s), engineer(s), and intern(s). The web app created will displayed all necessary information and allow the user to email any employee that it chooses with the press of a button.",
+    image: "./project-1.gif",
+    link: "https://github.com/mbatorek7/hw10-team-profile",
+  },
+  {
+    title: "Note Taker",
+    subtitle: "Express",
+    description:
+      "With this application, the user can create a list of notes. Each note is displayed on the side of the screen. When the user clicks on the note, they can see the title and text that was written. The user's notes will be saved unless the user presses the delete button.",
+    image: "./project-2.gif",
+    link: "https://batorek-note-taker.herokuapp.com/",
+  },
+  {
+    title: "E-commerce Back End",
+    subtitle: "Object-relational mapping",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://github.com/mbatorek7/hw13-ORM",
+  },
+  {
+    title: "Work Day Scheduler",
+    subtitle: "Third-Party APIs",
+    description:
+      "This application is designed to let you easily manage your busy workday schedule. It allows you to see the exact date and time at the top. With that in mind, you will also be able to see which timeslots have past, which one is the present, and which timeslots are upcoming based on the colors. The user can input their tasks into a timeslot, but the task will not be saved unless the user presses the save button. Once saved, the user can close the application and reopen it later to see their tasks. If the user wishes to clear their schedule for the day, they can do so with the clear schedule button at the top of the page.",
+    image: "./project-4.gif",
+    link: "https://mbatorek7.github.io/hw5-workday-scheduler/",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "HTML/CSS",
+  "Node",
+  "MySQL",
+  "GitHub",
+  "MATLAB",
+];
